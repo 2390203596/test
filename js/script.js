b98b7dbc9a055ea94904416c380c35c5cf24ee8b
2390203596 <31237597+2390203596@users.noreply.github.com>
2019-12-15 10:55:03 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$("#sy").click(function () {
+	$(location).attr("href","万全首页.html")
+})
+$("#zpzg").click(function () {
+	$(location).attr("href","租凭招工.html")
+})
+$("#qyfc").click(function () {
+	$(location).attr("href","企业风采.html")
+})
+$("#grzx").click(function () {
+	$(location).attr("href","个人中心.html")
+})
+$(".qyrz").click(function(){
+	$(location).attr("href","企业入驻.html")
+})
+$(".scbutton").click(function () {
+	$(location).attr("href","万全首页.html")
+})
